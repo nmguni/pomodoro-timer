4652b500b5b13c65f2de36b0f8e1c20128fe25dc
Ndumiso Mguni <Ndumiso29@gmail.com>
2022-09-12 09:14:43 -0400

    pomodoro HTML skeleton complete

---
+++ index.js
@@ -0,0 +1,36 @@
+// Gloabl Variables 
+const open_modal = document.querySelector('settings__modal') 
+const close_modal = document.querySelector('.close_modal')
+const modal = document.querySelector('#myModal')
+
+const pomo_start = document.querySelector('.pomadoro__start') 
+const short_break = document.querySelector('.short__break') 
+const long_break = document.querySelector('.long__break');
+
+const time = document.querySelector('pomo__clock') 
+
+const start = document.querySelector('#btn__start') 
+const stop = document.querySelector('#btn__stop') 
+const reset = document.querySelector('#btn__reset');
+
+
+let counter = 0;
+let interval = null;
+
+// event listeners 
+
+// Pomodoro button - rests time to 25:00
+// short break 
+// long break
+
+//  Start Button 
+// Stop Button
+// Reset Button - resets time to chosen time
+
+
+// Open Settings modal https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_modal
+
+//  OPEN MODAL 
+// open_modal.addEventListener('click', )
+//  CLOSE MODAL
+// CLOSE WHEN CLICKED OUTSIDE OF MODAL 
